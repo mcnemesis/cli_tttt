12eee8f4de3b07ef9c3a1ca08324ccd1791a2896
JWL <joewillirich@gmail.com>
2024-06-11 12:23:04 +0300

    create a self-installation option for arbitrary systems supporting bash and *.deb packages

---
+++ package/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+PACKAGE_URI=https://github.com/mcnemesis/cli_tttt/releases/download/tttt-1.0.1/tttt_1.0.1_amd64.deb
+PACKAGE_FILE=package.deb
+wget -O $PACKAGE_FILE $PACKAGE_URI && sudo dpkg -i $PACKAGE_FILE
+if command -v tttt &> /dev/null
+then
+	echo "TTTT installation was successful."
+    rm $PACKAGE_FILE
+	echo "=====-------||| WELCOME TO TEA ||-------====="
+	# so we can immediately test if it is working fine
+	tttt -h
+	echo "=====-----------------------------------====="
+else
+	echo "=====----<< THANKS FOR LOVING TEA >>----====="
+	echo "TTTT installation was not successful"
+	echo "=====-----------------------------------====="
+    [ -f "$PACKAGE_FILE" ] && rm "$PACKAGE_FILE"
+fi
