548f5b69b9d2cc4c1c6394bf67e90062f3595041
JWL <joewillirich@gmail.com>
2026-03-26 20:53:33 +0300

    essential updates: docs and bump IDE version

--- web/web_tea/app.js
+++ web/web_tea/app.js
@@ -12,7 +12,7 @@ import { TEA_RunTime as TEA } from './tea.js';
 
 console.log(U.test());
 
-var IDE_VERSION = '2.1.3';
+var IDE_VERSION = '2.1.4';
 var DEBUG = false;
 var DARK_UI = false;
 var DARK_UI_CLASS = "dark";
