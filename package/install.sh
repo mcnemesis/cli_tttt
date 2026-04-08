6587279d9064cae18b37f7c08a1b53a38b6c8179
JWL <joewillirich@gmail.com>
2026-04-08 11:35:55 +0300

    TTTT v1.4.8 package now ready

--- package/install.sh
+++ package/install.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-PACKAGE_URI=https://github.com/mcnemesis/cli_tttt/releases/download/tttt-1.4.3/tttt_1.4.3_amd64.deb
+PACKAGE_URI=https://github.com/mcnemesis/cli_tttt/releases/download/tttt-1.4.8/tttt_1.4.8_amd64.deb
 PACKAGE_FILE=package.deb
 wget -O $PACKAGE_FILE $PACKAGE_URI && sudo dpkg -i $PACKAGE_FILE
 if command -v tttt &> /dev/null
 then
-	echo "TTTT, ZHA & AMC installation was successful."
+	echo "TTTT, ZHA, AMC & Lexicon installation was successful."
     rm $PACKAGE_FILE
 	echo "=====-------||| WELCOME TO TEA ||-------====="
 	echo "INSTALLED: tttt <- command to run TEA programs"
 	echo "INSTALLED: zha <- command to run TEA app: Zee Hacker Assistant, a chatbot"
 	echo "INSTALLED: amc <- command to run TEA app: Advanced Mathematics Computer, a calculator"
+	echo "INSTALLED: lexicon <- command to run TEA app: Free Lexicon, a dictionary"
 	echo "=====-------||| ENJOY TEA ||-------====="
 	# so we can immediately test if it is working fine
 	tttt -h
