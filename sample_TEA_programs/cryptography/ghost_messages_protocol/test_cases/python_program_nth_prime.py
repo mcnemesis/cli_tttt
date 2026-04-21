1b048fadb6973577446cb3a3af7c4b08b597f07c
JWL <joewillirich@gmail.com>
2026-04-21 14:58:04 +0300

    Ghost Messages has been updated to v1.3 and has been tested on complex inputs now included in examples

---
+++ sample_TEA_programs/cryptography/ghost_messages_protocol/test_cases/python_program_nth_prime.py
@@ -0,0 +1,21 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def nth_prime(n):
+    count = 0
+    num = 1
+    while count < n:
+        num += 1
+        if is_prime(num):
+            count += 1
+    return num
+
+# Example usage
+n = int(input("Enter n (to find the nth prime): "))
+print(f"The {n}th prime number is {nth_prime(n)}")
+
